e6a9d4bd41e1fa8c9cf2c25199ce65350caeae24
jadecodes8191 <jadecodes8191@gmail.com>
2020-02-09 09:35:27 -0500

    initial commit -- opencv works

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import pytesseract
+
+TESSERACT_PATH = ""
+IMG_PATH = ""
+
+pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+img = cv2.imread(IMG_PATH)
+
+text = pytesseract.image_to_string(img)
+print(text)
+
+
